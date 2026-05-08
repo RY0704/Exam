38d411bd7311ef72c372a87f3eea66a7cb41ec91
a12 <ooi2572046@stu.o-hara.ac.jp>
2026-05-08 14:50:25 +0900

    a

--- src/main/java/dao/TestDao.java
+++ src/main/java/dao/TestDao.java
@@ -1,5 +1,5 @@
 package dao;
-
+ 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,42 +7,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import bean.School;
-<<<<<<< HEAD
-=======
 import bean.Student;
 import bean.Subject;
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
 import bean.Test;
-
+ 
 public class TestDao extends Dao {
-
-<<<<<<< HEAD
-    public Test get(String studentNo, String subjectCd, School school, int no) throws Exception {
-        Test test = null;
-        Connection connection = getConnection();
-        PreparedStatement statement = null;
-=======
+ 
     /**
      * クラス図に基づいたベースSQL
      * studentテーブルをJOINし、名前や入学年度も取得できるようにしています
      */
-    private final String baseSql = 
+    private final String baseSql =
         "select t.*, s.student_name, s.ent_year " +
         "from test t " +
         "join student s on t.student_no = s.student_no " +
         "and t.school_cd = s.school_cd ";
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-        try {
-            String sql = "select * from test where student_no=? and subject_cd=? and school_cd=? and no=?";
-            statement = connection.prepareStatement(sql);
-            statement.setString(1, studentNo);
-            statement.setString(2, subjectCd);
-            statement.setString(3, school.getSchoolCd());
-            statement.setInt(4, no);
-            ResultSet resultSet = statement.executeQuery();
-=======
+ 
     /**
      * 指定された条件で1件の成績情報を取得する
      */
@@ -50,25 +30,7 @@ public class TestDao extends Dao {
         Test test = null;
         Connection connection = getConnection();
         PreparedStatement statement = null;
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-            if (resultSet.next()) {
-                test = new Test();
-                test.setStudentNo(resultSet.getString("student_no"));
-                test.setSubjectCd(resultSet.getString("subject_cd"));
-                test.setClassNum(resultSet.getString("class_num"));
-                test.setNo(resultSet.getInt("no"));
-                test.setPoint(resultSet.getInt("point"));
-                test.setSchool(school);
-            }
-        } finally {
-            if (statement != null) statement.close();
-            if (connection != null) connection.close();
-        }
-        return test;
-    }
-=======
+ 
         try {
             // ベースSQLに条件を追加（テーブル名 t. を明示）
             String sql = baseSql + "where t.student_no=? and t.subject_cd=? and t.school_cd=? and t.no=?";
@@ -80,42 +42,7 @@ public class TestDao extends Dao {
             
             ResultSet rSet = statement.executeQuery();
             List<Test> list = postFilter(rSet, school);
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private List<Test> postFilter(ResultSet resultSet, School school) throws Exception {
-        List<Test> list = new ArrayList<>();
-        while (resultSet.next()) {
-            Test test = new Test();
-            test.setStudentNo(resultSet.getString("student_no"));
-            test.setSubjectCd(resultSet.getString("subject_cd"));
-            test.setNo(resultSet.getInt("no"));
-            test.setPoint(resultSet.getInt("point"));
-            test.setClassNum(resultSet.getString("class_num"));
-            test.setSchool(school);
-            list.add(test);
-        }
-        return list;
-    }
-=======
-		return test;
-	}
-	private List<Test> postFilter(ResultSet resultSet, School school) throws Exception {
-	    List<Test> list = new ArrayList<>();
-	    try {
-	        while (resultSet.next()) {
-	            Test test = new Test();
-	            // Test Beanのフィールドに合わせてセット
-	            test.setStudentNo(resultSet.getString("student_no"));
-	            test.setSubjectCd(resultSet.getString("subject_cd"));
-	            test.setNo(resultSet.getInt("no"));
-	            test.setPoint(resultSet.getInt("point"));
-	            test.setClassNum(resultSet.getString("class_num"));
-	            test.setSchool(school);
-	            
-	            list.add(test);
-=======
+ 
             if (!list.isEmpty()) {
                 test = list.get(0);
             }
@@ -125,25 +52,7 @@ public class TestDao extends Dao {
         }
         return test;
     }
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	        throw e;
-	    }
-	    return list;
-	}
-	public List<Test> filter(
-	        int entYear,
-	        String classNum,
-	        Subject subject,
-	        int num,
-	        School school
-	) throws Exception {
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-=======
+ 
     /**
      * 検索条件（入学年度、クラス、科目、回数）に合致する成績一覧を取得する
      */
@@ -151,77 +60,31 @@ public class TestDao extends Dao {
         List<Test> list = new ArrayList<>();
         Connection connection = getConnection();
         PreparedStatement statement = null;
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-    public List<Test> filter(int entYear, String classNum, String subjectCd, int num, School school) throws Exception {
-        List<Test> list = new ArrayList<>();
-        Connection connection = getConnection();
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-=======
+ 
         try {
             // SQL文：IS_ATTENDフラグも含めてチェック
-            String sql = baseSql + 
+            String sql = baseSql +
                 "where s.ent_year = ? and s.class_num = ? and t.subject_cd = ? " +
                 "and t.no = ? and t.school_cd = ? and s.is_attend = true " +
                 "order by t.student_no";
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-        try {
-            // 【修正点】s.is_attend = true の条件を削除しました
-            String sql =
-            	    "select t.* " +
-            	    "from test t " +
-            	    "join student s on t.student_no = s.student_no " +
-            	    "and t.school_cd = s.school_cd " +
-            	    "where s.ent_year = ? " +
-            	    "and s.class_num = ? " +
-            	    "and t.subject_cd = ? " +
-            	    "and t.no = ? ";
-=======
+ 
             statement = connection.prepareStatement(sql);
             statement.setInt(1, entYear);
             statement.setString(2, classNum);
             statement.setString(3, subject.getSubjectCd());
             statement.setInt(4, num);
             statement.setString(5, school.getSchoolCd());
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-            statement = connection.prepareStatement(sql);
-            statement.setInt(1, entYear);
-            statement.setString(2, classNum);
-            statement.setString(3, subjectCd);
-            statement.setInt(4, num);
-            statement.setString(5, school.getSchoolCd());
-=======
+ 
             ResultSet rSet = statement.executeQuery();
             list = postFilter(rSet, school);
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-            resultSet = statement.executeQuery();
-            list = postFilter(resultSet, school);
-=======
+ 
         } finally {
             if (statement != null) statement.close();
             if (connection != null) connection.close();
         }
         return list;
     }
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-        } finally {
-            if (resultSet != null) resultSet.close();
-            if (statement != null) statement.close();
-            if (connection != null) connection.close();
-        }
-        return list;
-    }
-=======
+ 
     /**
      * ResultSetから得られた情報をTestオブジェクトに詰め替える（Student情報も紐付け）
      */
@@ -235,32 +98,7 @@ public class TestDao extends Dao {
             test.setPoint(rSet.getInt("point"));
             test.setClassNum(rSet.getString("class_num"));
             test.setSchool(school);
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-    public boolean save(List<Test> list) throws Exception {
-        boolean result = true;
-        Connection connection = null;
-        try {
-            connection = getConnection();
-            connection.setAutoCommit(false);
-            for (Test test : list) {
-                if (!save(test, connection)) {
-                    result = false;
-                    break;
-                }
-            }
-            if (result) connection.commit();
-            else connection.rollback();
-        } catch (Exception e) {
-            if (connection != null) connection.rollback();
-            throw e;
-        } finally {
-            if (connection != null) connection.close();
-        }
-        return result;
-    }
-=======
+ 
             // 学生(Student) Beanを作成し、Test Beanにセットする（JSP表示用）
             Student student = new Student();
             student.setStudentNo(rSet.getString("student_no"));
@@ -270,55 +108,12 @@ public class TestDao extends Dao {
             
             // 重要：Test.javaに作成した setStudent メソッドを呼び出す
             test.setStudent(student);
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-    public boolean save(Test test, Connection connection) throws Exception {
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        try {
-            String checkSql = "select count(*) from test where student_no = ? and subject_cd = ? and school_cd = ? and no = ?";
-            statement = connection.prepareStatement(checkSql);
-            statement.setString(1, test.getStudentNo());
-            statement.setString(2, test.getSubjectCd());
-            statement.setString(3, test.getSchool().getSchoolCd());
-            statement.setInt(4, test.getNo());
-            resultSet = statement.executeQuery();
-            resultSet.next();
-            boolean exists = resultSet.getInt(1) > 0;
-            statement.close();
-=======
+ 
             list.add(test);
         }
         return list;
     }
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
-
-<<<<<<< HEAD
-            if (exists) {
-                String updateSql = "update test set point = ?, class_num = ? where student_no = ? and subject_cd = ? and school_cd = ? and no = ?";
-                statement = connection.prepareStatement(updateSql);
-                statement.setInt(1, test.getPoint());
-                statement.setString(2, test.getClassNum());
-                statement.setString(3, test.getStudentNo());
-                statement.setString(4, test.getSubjectCd());
-                statement.setString(5, test.getSchool().getSchoolCd());
-                statement.setInt(6, test.getNo());
-            } else {
-                String insertSql = "insert into test(student_no, subject_cd, school_cd, no, point, class_num) values(?,?,?,?,?,?)";
-                statement = connection.prepareStatement(insertSql);
-                statement.setString(1, test.getStudentNo());
-                statement.setString(2, test.getSubjectCd());
-                statement.setString(3, test.getSchool().getSchoolCd());
-                statement.setInt(4, test.getNo());
-                statement.setInt(5, test.getPoint());
-                statement.setString(6, test.getClassNum());
-            }
-            statement.executeUpdate();
-            return true;
-        } finally {
-            if (resultSet != null) resultSet.close();
-=======
+ 
     /**
      * 成績情報のリストを一括保存（登録または更新）する
      */
@@ -346,7 +141,7 @@ public class TestDao extends Dao {
         }
         return result;
     }
-
+ 
     /**
      * 1件の成績情報を保存する
      */
@@ -363,11 +158,10 @@ public class TestDao extends Dao {
             statement.setInt(4, test.getNo());
             statement.setInt(5, test.getPoint());
             statement.setString(6, test.getClassNum());
-
+ 
             statement.executeUpdate();
             return true;
         } finally {
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
             if (statement != null) statement.close();
         }
     }
--- src/main/java/scoremanager/main/TestRegistExecuteAction.java
+++ src/main/java/scoremanager/main/TestRegistExecuteAction.java
@@ -1,6 +1,6 @@
 package scoremanager.main;
-
-<<<<<<< HEAD
+ 
+ 
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,107 +12,36 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import tool.Action;
-
+ 
 public class TestRegistExecuteAction extends Action {
-
+ 
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
         HttpSession session = req.getSession();
         Teacher teacher = (Teacher) session.getAttribute("user");
         School school = teacher.getSchool();
-
-        // 1. 検索条件（科目、回数、クラス）をリクエストから取得
-        // ※ JSPのform内に <input type="hidden"> で持たせておく必要があります
-        String subjectCd = req.getParameter("f3"); // 科目コード
-        int num = Integer.parseInt(req.getParameter("f4")); // 回数
-        String classNum = req.getParameter("f2"); // クラス番号
-
-        // 2. 画面から送られてくる「学籍番号のリスト」と「点数のリスト」を取得
-        // JSP側で name="student_no_set[]" および name="point_set[]" としている前提
-        String[] studentNoSet = req.getParameterValues("student_no_set[]");
-        String[] pointSet = req.getParameterValues("point_set[]");
-
-        List<Test> testList = new ArrayList<>();
-        TestDao tDao = new TestDao();
-
-        if (studentNoSet != null && pointSet != null) {
-            // 学生の数だけループしてTestインスタンスを作成
-            for (int i = 0; i < studentNoSet.length; i++) {
-                Test test = new Test();
-                
-                // カラム名に対応するフィールドへセット
-                test.setStudentNo(studentNoSet[i]); // student_no
-                test.setSubjectCd(subjectCd);       // subject_cd
-                test.setNo(num);                    // no (回数)
-                test.setSchool(school);             // school_cd用
-                test.setClassNum(classNum);         // class_num
-                
-                // 点数のパース処理
-                int point = 0;
-                if (pointSet[i] != null && !pointSet[i].isEmpty()) {
-                    point = Integer.parseInt(pointSet[i]);
-                }
-                test.setPoint(point);               // point
-
-                testList.add(test);
-            }
-        }
-
-        // 3. DAOを使って一括保存
-        try {
-            // TestDaoの save(List<Test>) メソッドを呼び出し
-            tDao.save(testList);
-            
-            // 登録完了後は完了画面へ
-            req.getRequestDispatcher("test_regist_done.jsp").forward(req, res);
-        } catch (Exception e) {
-            e.printStackTrace();
-            // エラー時はエラーページへ（error.jspがない場合は作成するか、適切なページへ）
-            req.getRequestDispatcher("/scoremanager/main/error.jsp").forward(req, res);
-        }
-=======
-
-import java.util.ArrayList;
-import java.util.List;
-
-import bean.School;
-import bean.Teacher;
-import bean.Test;
-import dao.TestDao;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import tool.Action;
-
-public class TestRegistExecuteAction extends Action {
-
-    @Override
-    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-        HttpSession session = req.getSession();
-        Teacher teacher = (Teacher) session.getAttribute("user");
-        School school = teacher.getSchool();
-
+ 
         // 1. どの科目の何回目か、保存に必要な情報を取得
         // ※JSPの検索パラメータ(f3, f4)が hidden などで送られている前提
         String subjectCd = req.getParameter("f3");
         int num = Integer.parseInt(req.getParameter("f4"));
         String classNum = req.getParameter("f2");
-
+ 
         TestDao tDao = new TestDao();
         List<Test> testList = new ArrayList<>();
-
+ 
         // 2. 画面から送られてきた全てのパラメータを確認し、点数を抽出
         java.util.Enumeration<String> names = req.getParameterNames();
         while (names.hasMoreElements()) {
             String name = names.nextElement();
-
+ 
             // "point_" で始まるパラメータ（点数入力欄）があれば処理
             if (name.startsWith("point_")) {
                 // 学籍番号を抽出
                 String studentNo = name.replace("point_", "");
                 // 入力された点数を取得
                 int point = Integer.parseInt(req.getParameter(name));
-
+ 
                 // 3. Testオブジェクトを組み立てる（クラス図準拠）
                 Test test = new Test();
                 test.setStudentNo(studentNo); // 実際の実装に合わせてstudentオブジェクトにする場合は要調整
@@ -121,16 +50,16 @@ public class TestRegistExecuteAction extends Action {
                 test.setPoint(point);
                 test.setClassNum(classNum);
                 test.setSchool(school);
-
+ 
                 testList.add(test);
             }
         }
-
+ 
         // 4. DAOを使って一括保存
         if (!testList.isEmpty()) {
             tDao.save(testList);
         }
-
+ 
         // 5. 完了メッセージを添えて元の画面へ
         req.setAttribute("message", "登録が完了しました。");
         // 検索状態を維持するためにパラメータを戻す
@@ -138,8 +67,6 @@ public class TestRegistExecuteAction extends Action {
         req.setAttribute("f2", classNum);
         req.setAttribute("f3", subjectCd);
         req.setAttribute("f4", num);
-        
         req.getRequestDispatcher("test_regist_done.jsp").forward(req, res);
->>>>>>> branch 'master' of https://github.com/RY0704/Exam.git
     }
 }
